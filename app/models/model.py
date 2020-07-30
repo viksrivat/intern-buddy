e8d4ed0d4487b4097ff726cfdbd43c5616c9f8a0
vikranth22446 <rama22446@gmail.com>
2020-07-30 14:48:46 -0700

    Update db

--- app/models/model.py
+++ app/models/model.py
@@ -18,22 +18,33 @@ class GROUP_SIZE:
     medium = 1
     high = 2
 
+class Position:
+    pm = 0
+    ba = 1
+    sde = 2
+
 class User(db.Model):
     __table_args__ = {'extend_existing': True}
-    record_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
-    log = db.Column(db.String(140))
+    user_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
     created_date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-
+    paired = db.Column(db.Boolean)
+    group_id = db.Column(db.Integer, db.ForeignKey('Group.group_id'))
+    group = relationship("Group", back_populates="users")
+    
+class Group(db.Model)
+    __table_args__ = {'extend_existing': True}
+    group_id = db.Column(db.Integer, primary_key=True, unique=True, autoincrement=True)
+    groups = relationship("User", back_populates="group", uselist=True)
 
 class Preference(db.Model):
     __table_args__ = {'extend_existing': True}
     extend_existing = True
-    preference_id = db.Column(db.Integer, db.ForeignKey('record.record_id'))
+    preference_id = db.Column(db.Integer, db.ForeignKey('User.user_id'))
+
     location = db.Column(db.String)
     school = db.Column(db.String)
     group_size = db.Column(db.Integer)
-    position_type = db.Column(db.String)
+    position_type = db.Column(db.Integer)
     age = db.Column(db.Integer)
 
-    school_level = db.Column(db.Integer)
     hangout_outside = db.Column(db.String)
