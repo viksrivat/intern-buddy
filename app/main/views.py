8c6c358e21937c52c5a6eca97e939762b0c1ba2c
root <root@ip-172-31-59-120.ec2.internal>
2020-07-30 23:58:13 +0000

    Update alexa and views for endpoint

--- app/main/views.py
+++ app/main/views.py
@@ -6,6 +6,12 @@ from app import db
 from app.main import main
 import time
 
+@main.route("/session_info", methods=["POST", "GET"])
+def session_details():
+    content = request.json
+    print(content)
+
+
 @main.route("/")
 def index():
     return render_template('index.html')
